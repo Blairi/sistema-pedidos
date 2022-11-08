41c9ac29d50668480bf652119fed3a929dd7c017
Blairi <axel.avi.fer@gmail.com>
2022-11-07 18:02:14 -0600

    entidad cliente

---
+++ dominio/Cliente.py
@@ -0,0 +1,15 @@
+
+class Cliente:
+
+    def __init__(self, id:int, nombre:str) -> None:
+        self.id = id
+        self.nombre = nombre
+    
+    def get_id(self) -> int:
+        return self.id
+
+    def get_nombre(self) -> str:
+        return self.nombre
+
+    def __str__(self) -> str:
+        return f"id: {self.id}\nnombre: {self.nombre}"
