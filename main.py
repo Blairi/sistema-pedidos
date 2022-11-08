0373a9d9841aaef355fdbe90263c53461ed8d244
Blairi <axel.avi.fer@gmail.com>
2022-11-07 18:04:00 -0600

    agregando el controlador de clientes

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ sys.path.insert(0,"..")
 
 from controladores.GrafosControlador import GrafosControlador
 from controladores.ProductosControlador import ProductosControlador
+from controladores.ClientesControlador import ClientesControlador
 
 from helpers.limpiar_pantalla import limpiar_pantalla
 
 def main():
+
     producto_controlador = ProductosControlador()
     grafo_controlador = GrafosControlador()
+    cliente_controlador = ClientesControlador()
 
     while True:
 
         print("===== Sistema de pedidos =====")
         print("Eligé escribiendo el número de la opción deseada:")
 
-        opc = int(input("0. Salir.\n1. Mi mapa.\n2. Mis productos.\n: "))
+        opc = int(input("0. Salir.\n1. Mi mapa.\n2. Mis productos.\n3. Clientes.\n: "))
 
         if opc == 0:
             break
@@ -28,6 +31,10 @@ def main():
         if opc == 2:
             limpiar_pantalla()
             producto_controlador.menu()
+        
+        if opc == 3:
+            limpiar_pantalla()
+            cliente_controlador.menu()
 
 
 main()
